90a981548b9238f2fa9782a653acc558d9eba154
Elnara <elutic@gmail.com>
2016-11-12 23:18:33 +0300

    update

--- app/directives/directives.js
+++ app/directives/directives.js
@@ -2,85 +2,98 @@
     "use strict";
 
     angular.module("app")
-        .directive("unorderedList1", unorderedList1)
-        .directive("unorderedList2", unorderedList2)
-        .directive("unorderedList3", unorderedList3)
-        .directive("unorderedList4", unorderedList4)
-        .directive("unorderedList5", unorderedList5);
+      .directive("unorderedList1", unorderedList1)
+      .directive("unorderedList2", unorderedList2)
+      .directive("unorderedList3", unorderedList3)
+      .directive("unorderedList4", unorderedList4)
+      .directive("unorderedList5", unorderedList5)
+      .directive("unorderedListAttrs", unorderedListAttrs)
+      .directive("unorderedListScope", unorderedListScope)
+    ;
 
-    function unorderedList1 () {
-        return function (scope) { //controller scope
+    function unorderedList1() {
+        return function(scope) {
             let data = scope.products;
 
             angular.forEach(data, (dataItem) => {
                 console.log(`Item: ${dataItem.name}`);
-            });
-        }
+        });
+        };
     }
 
-    function unorderedList2 () {
+    function unorderedList2() {
         return {
-            link: function (scope) { //controller scope
+            link: function(scope) {
                 let data = scope.products;
 
                 angular.forEach(data, (dataItem) => {
                     console.log(`Item: ${dataItem.name}`);
-                });
+            });
             }
-        }
+        };
     }
 
-    function unorderedList3 () {
+    function unorderedList3() {
         return {
-            link: function (scope, elem, attrs) {
+            link: function(scope, elem, attrs) {
                 let data = scope[attrs["unorderedList3"]],
-                    propertyName = attrs["listProperty"];
+                  propertyName = attrs["listProperty"];
 
                 angular.forEach(data, (dataItem) => {
-                    console.log(`Item: ${dataItem[propertyName]}`); //srting template literal
-                });
+                    // console.log(`Item: ${dataItem[propertyName]}`);
+                    console.log(`Item: ${scope.$eval(propertyName, dataItem)}`);
+            });
             }
-        }
+        };
     }
 
-    function unorderedList4 () {
+    function unorderedList4() {
         return {
-            link: function (scope, elem, attrs) {
+            link: function(scope, elem, attrs) {
                 let data = scope[attrs["unorderedList4"]],
-                    propertyName = attrs["listProperty"];
+                  propertyName = attrs["listProperty"];
 
                 let listElem = angular.element("<ul>");
 
                 angular.forEach(data, (dataItem) => {
                     listElem.append( angular.element("<li>")
-                        .text(scope.$eval(propertyName, dataItem)))
-                });
+                  .text(scope.$eval(propertyName, dataItem)) );
+            });
 
                 elem.append(listElem);
             }
-        }
+        };
     }
 
     function unorderedList5() {
         return {
             link: function(scope, elem, attrs) {
                 let data = scope[attrs["unorderedList5"]],
-                    propertyName = attrs["listProperty"];
+                  propertyName = attrs["listProperty"];
 
                 let listElem = angular.element("<ul>");
 
                 angular.forEach(data, (dataItem) => {
                     let itemElem = angular.element("<li>");
-                    listElem.append(itemElem);
+                listElem.append(itemElem);
 
-                    let watcherFn = function(watchScope) {
-                        return watchScope.$eval(propertyName, dataItem);
-                    };
+                /*let watcherFn = function(watchScope) {
+                 return watchScope.$eval(propertyName, dataItem);
+                 };
 
-                    scope.$watch(watcherFn, (newVal, oldVal) => {
-                        itemElem.text(newVal);
-                    });
-                });
+                 scope.$watch(watcherFn, (newVal, oldVal) => {
+                 itemElem.text(newVal);
+                 });*/
+
+                let watcherFn = function(watchScope) {
+                    return dataItem;
+                };
+
+                scope.$watchCollection(watcherFn, (newVal, oldVal) => {
+                    itemElem.text( scope.$eval(propertyName, newVal) );
+            });
+
+            });
 
                 elem.append(listElem);
             }
@@ -90,44 +103,72 @@
     function unorderedListAttrs() {
         return {
             link: function(scope, elem, attrs) {
-                let data = scope[attrs["unorderedListAttrs"]],
-                    propertyName = attrs["listProperty"];
+                let data = scope[attrs["unorderedListAttrs"]];
+
+                attrs.$observe("listProperty", (newVal, oldVal) => {
+                    elem.html("");
+
+                let propertyName = newVal;
 
                 let listElem = angular.element("<ul>");
 
                 angular.forEach(data, (dataItem) => {
                     let itemElem = angular.element("<li>");
-                    listElem.append(itemElem);
+                listElem.append(itemElem);
 
-                    let watcherFn = function(watchScope) {
-                        return watchScope.$eval(propertyName, dataItem);
-                    };
+                let watcherFn = function(watchScope) {
+                    return dataItem;
+                };
 
-                    scope.$watch(watcherFn, (newVal, oldVal) => {
-                        itemElem.text(newVal);
-                    });
-                });
+                scope.$watchCollection(watcherFn, (newVal, oldVal) => {
+                    itemElem.text( scope.$eval(propertyName, newVal) );
+            });
+
+            });
 
                 elem.append(listElem);
+
+            });
             }
         };
     }
 
+    function unorderedListScope() {
+        return {
+            scope: {
+                unorderedListScope: "=",
+                listProperty: "="
+            },
+            link: function(scope, elem, attrs) {
+                let data = scope["unorderedListScope"];
+
+                scope.$watch("listProperty", (newVal, oldVal) => {
+                    elem.html("");
+
+                let propertyName = newVal;
+
+                let listElem = angular.element("<ul>");
+
+                angular.forEach(data, (dataItem) => {
+                    let itemElem = angular.element("<li>");
+                listElem.append(itemElem);
+
+                let watcherFn = function(watchScope) {
+                    return dataItem;
+                };
+
+                scope.$watchCollection(watcherFn, (newVal, oldVal) => {
+                    itemElem.text( scope.$eval(propertyName, newVal) );
+            });
+
+            });
+
+                elem.append(listElem);
+
+            });
+            }
+        };
+    }
+
+
 })();
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
